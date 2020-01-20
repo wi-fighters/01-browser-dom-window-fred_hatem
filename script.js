ac159a700bffeeb44aef63dfddca4c83dc9141cf
HatemZreek88 <hatem.zreek88@gmail.com>
2020-01-20 11:34:11 +0100

    user guess

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (() => {
   let randomNumber = Math.floor(Math.random() * 10) + 1;
-  //   let userInput = prompt("Please, guess a number between 1 - 10:");
+  let userInput = prompt("Please, guess a number between 1 - 10:");
   //   let attempts = 3;
   //   if (randomNumber)
 })();
